a1e588e457b56bbdf4d18adaa6cb3def8de02457
Valon Kastrati <valon.k@outlook.at>
2022-04-17 18:44:26 +0200

    new name with as, bring into scope

--- pkg_crates_mod/restaurant/src/lib.rs
+++ pkg_crates_mod/restaurant/src/lib.rs
@@ -1,3 +1,14 @@
+// give the mod a new name with the [as] keyword
+use crate::front_of_house::hosting as Hosting;
+
+// bring absolute path into scope
+use crate::front_of_house::hosting;
+
+//
+//  bring relative path into scope use self::front_of_house::hosting;
+
+use back_of_house::Appetizer;
+
 mod front_of_house{
     pub mod hosting{
         pub fn add_two_waitlist(){}
@@ -17,14 +28,14 @@ pub fn eat_at_restaurant(){
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
 
-    let order1 = back_of_seat::Appetizer::Soup;
-    let order2 = back_of_seat::Appetizer::Salad;
+    let order1 = Appetizer::Soup;
+    let order2 = Appetizer::Salad;
 
     // Absolute path
-    crate::front_of_house::hosting::add_two_waitlist();
+    hosting::add_two_waitlist();
 
     // Relative Path
-    front_of_house::hosting::add_two_waitlist();
+    hosting::add_two_waitlist();
 }
 
 fn serve_order(){}
